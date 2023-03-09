1830971b023a503d769a492e2baef156619e8a3d
Ahmed <27med101@gmail.com>
2023-03-09 13:57:19 +0200

    Starting to code today, so cool

---
+++ 2.sh
@@ -0,0 +1,26 @@
+cd 0x01-git
+mkdir bash 
+mkdir c 
+mkdir js
+touch c/c_is_fun.c
+
+touch js/main.js
+
+touch js/index.js
+
+cd c 
+
+ls 
+
+cd ..  
+cd bash 
+
+echo '#!/bin/bash' >alx
+
+echo 'ALX' >>alx
+echo '#!/bin/bash'>school
+echo 'School'>>school
+cd .. 
+git add *
+git commit -m 'Starting to code today, so cool'
+git push 
